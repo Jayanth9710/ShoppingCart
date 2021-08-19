a540dd9b98a0aa8705dd70e930baecf10d24f06a
Jayanth9710 <kumarjayanth55@gmail.com>
2021-08-19 19:46:57 +0530

    Collapse function
    Navbar collapse fixed.

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
         <div className="container px-4 px-lg-5">
             <div className="navbar-brand fs-1">Shopping Page</div>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse show" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                     <Link className="nav-item nav-link active fs-5"  to="/">Home</Link>
                     <li className="nav-item dropdown">
